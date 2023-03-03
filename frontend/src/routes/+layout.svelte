5be3326d5d8462e906b22e33d92e0ffe59fa7447
Linh Dang <linhdanghoang@gmail.com>
2023-03-03 01:10:33 -0500

    Please commit

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,22 +1,24 @@
 <script>
-    import "@fontsource/petrona";
-    import "../app.css";
-    import Sidebar from "$lib/components/Sidebar.svelte";
-    import Navbar from "$lib/components/Navbar.svelte";
-    import Reader from "$lib/components/wrapper/Reader.svelte";
-    import Footer from "$lib/components/Footer.svelte";
+  import "@fontsource/petrona";
+  import "../app.css";
+  import Sidebar from "$lib/components/Sidebar.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
+  import Reader from "$lib/components/wrapper/Reader.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+
+  // Set up URQL client connecting to the Strapi GraphQL API
+  
 </script>
 
 <div class="min-h-screen">
-    <Navbar />
-    <!-- Define the main section below the Navbar here -->
-    <!-- Temporarily set height to 72 -->
-    <div class="flex flex-col md:flex-row h-72">
-        <Sidebar />
-        <Reader>
-            <slot />
-        </Reader>
-    </div>
-    <Footer/>
-       
+  <Navbar />
+  <!-- Define the main section below the Navbar here -->
+  <!-- Temporarily set height to 72 -->
+  <div class="flex flex-col md:flex-row h-72">
+    <Sidebar />
+    <Reader>
+      <slot />
+    </Reader>
+  </div>
+  <Footer />
 </div>
