98975a06d89331377f6f82a18a975a186356698a
Linh Dang <linhdanghoang@gmail.com>
2023-03-21 01:07:23 -0400

    Added DaisyUI elements

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -9,7 +9,7 @@
             alt="Go to Teapot Reader Homepage"/>
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">TEAPOT READER</span>
         </a>
-        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <!-- <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
         </button>
@@ -19,6 +19,14 @@
                 <a href="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white" aria-current="page">Home</a>
             </li>
         </ul>
-        </div>
+        </div> -->
+        <div class="dropdown dropdown-end">
+            <label tabindex="-1" class="btn m-1">Menu</label>
+            <ul tabindex="-1" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a href="/">Home</a></li>
+              <li><a href="/chapters">Chapters</a></li>
+              <li><a href="/characters">Characters</a></li>
+            </ul>
+          </div>
     </div>
 </nav>
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -4,5 +4,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: false
+  }
+
 }
