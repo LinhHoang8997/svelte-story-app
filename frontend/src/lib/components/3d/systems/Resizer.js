20112292a66707b34157b4ccabfed0d423cb5a84
Linh Dang <linhdanghoang@gmail.com>
2023-04-08 18:31:36 -0400

    Add anti-aliasing and resizing to Three.js component

--- frontend/src/lib/components/3d/systems/Resizer.js
+++ frontend/src/lib/components/3d/systems/Resizer.js
@@ -1,15 +1,28 @@
+function setSize(container, camera, renderer) {
+  // Set the camera's aspect ratio
+  camera.aspect = container.clientWidth / container.clientHeight;
+  // update the camera's frustum
+  camera.updateProjectionMatrix();
+
+  // update the size of the renderer AND the canvas
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  // set the pixel ratio (for mobile devices)
+  renderer.setPixelRatio(window.devicePixelRatio);
+}
+
 class Resizer {
-    constructor(container, camera, renderer) {
-        // Set the camera's aspect ratio
-    camera.aspect = container.clientWidth / container.clientHeight;
-    // update the camera's frustum
-    camera.updateProjectionMatrix();
+  constructor(container, camera, renderer) {
+    setSize(container, camera, renderer);
 
-    // update the size of the renderer AND the canvas
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    // set the pixel ratio (for mobile devices)
-    renderer.setPixelRatio(window.devicePixelRatio);
-    }
+    window.addEventListener("resize", () => {
+      setSize(container, camera, renderer);
+
+      this.onResize();
+      //placeholder function that will be overwritten when defining the World class
+      // We do this because we want to call the render function of the World class
+
+    });
   }
+}
 
-  export { Resizer };
+export { Resizer };
--- frontend/src/lib/components/3d/systems/renderer.js
+++ frontend/src/lib/components/3d/systems/renderer.js
@@ -1,7 +1,9 @@
 import { WebGLRenderer } from 'three';
 
 function createRenderer() {
-  const renderer = new WebGLRenderer();
+  const renderer = new WebGLRenderer(
+    { antialias: true }
+  );
   // renderer.physicallyCorrectLights = true;
   // deprecated as this is the default now, change using the renderer.useLegacyLights instead
 
