222cd89488fbab6192115ab267479e766c827b06
Linh Dang <linhdanghoang@gmail.com>
2023-08-10 02:05:06 -0400

    Update +layout.svelte to detect authentication state change and invalidate session (rerun loads) based on auth state

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -8,9 +8,29 @@
   import MusicTrackCard from "$lib/components/reader/MusicTrackCard.svelte";
   import UtilityBar from "$lib/components/reader/UtilityBar.svelte";
 
+  import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+	
   export let data;
 
+  // Extract the data from the Houdini query response first - Content first
   $: ({ titles_data } = data);
+
+  // Extract the data from the Supabase session - Auth and User second
+	let { supabase, session } = data
+	$: ({ supabase, session } = data)
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth')
+			}
+		})
+
+		return () => data.subscription.unsubscribe()
+	})
+
+
 </script>
 
 <div class="min-h-screen">
